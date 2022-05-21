fde2b804b79ff158668bdd09143b360b59138165
adrian <105897044+adrianpachas@users.noreply.github.com>
2022-05-21 12:25:38 -0500

    vector ordenado

--- ord_burbuja.c
+++ ord_burbuja.c
@@ -5,6 +5,7 @@
 #define SIZE 10
 
 int main() {
+    int aux,i,j;
     int ord_vector[SIZE];
     srand(time(NULL));
     printf("Generacion de numeros aleatorios");
@@ -12,5 +13,18 @@ int main() {
         ord_vector[i]=100+rand()%11;
         printf("\n");
         printf("%d",ord_vector[i]);
+    }
+    
+    for(i=0;i<SIZE;i++){
+        for(j=0;j<SIZE-i-1;j++){
+            if(ord_vector[j]>ord_vector[j+1]){
+                if(ord_vector[j]>ord_vector[j+1]){
+                    aux=ord_vector[j];
+                    ord_vector[j]=ord_vector[j+1];
+                    ord_vector[j+1]=aux;
+
+                }
+            }
+        }
     }
 }
